17408cc0ff61fd95a8bcc3b464282a9ec7900bca
Yasindu Kalhara Balasooriya <124476484+Yasindu07@users.noreply.github.com>
2025-05-04 17:26:35 +0530

    Update API_URL in .env and authService.js to use production backend

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-const API_URL = process.env.APP_API_URL || "http://localhost:5001";
+const API_URL =
+  process.env.APP_API_URL || "https://country-app-backend-nu.vercel.app/";
 
 const authService = {
   login: async (email, password) => {
     try {
-      const response = await axios.post(`${API_URL}/api/v1/auth/sign-in`, {
+      const response = await axios.post(`${API_URL}api/v1/auth/sign-in`, {
         email,
         password,
       });
@@ -18,7 +19,7 @@ const authService = {
 
   register: async (name, email, password) => {
     try {
-      const response = await axios.post(`${API_URL}/api/v1/auth/sign-up`, {
+      const response = await axios.post(`${API_URL}api/v1/auth/sign-up`, {
         name,
         email,
         password,
@@ -38,7 +39,7 @@ const authService = {
         throw new Error("No token found");
       }
 
-      const response = await axios.get(`${API_URL}/api/v1/auth/me`, {
+      const response = await axios.get(`${API_URL}api/v1/auth/me`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
